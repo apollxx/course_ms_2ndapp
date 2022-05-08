fbceefd9a5d51664820beb27fb44eabb72bc75a2
Leonardo <lezera12@gmail.com>
2022-05-07 22:12:13 -0300

    client updated

--- client/components/header.js
+++ client/components/header.js
@@ -1,18 +1,20 @@
 import Link from 'next/link'
 
-const Header = ({currentUser}) => {
-const links = [
-    !currentUser && {label: 'Sign Up', href: '/auth/signup'},
-    !currentUser &&{label: 'Sign In', href: '/auth/signin'},
-    currentUser && {label: 'Sign Out', href: '/auth/signout'}
-].filter(linkConfig => linkConfig)
-.map(({label,href}) => {
-    return <li key={href}>
-    <Link href={href}>
-       <a className="nav-link"> {label} </a>
-    </Link>
-    </li>
-})
+const Header = ({ currentUser }) => {
+    const links = [
+        !currentUser && { label: 'Sign Up', href: '/auth/signup' },
+        !currentUser && { label: 'Sign In', href: '/auth/signin' },
+        currentUser && { label: 'Sell Tickets', href: '/tickets/new' },
+        currentUser && { label: 'My Orders', href: '/orders' },
+        currentUser && { label: 'Sign Out', href: '/auth/signout' }
+    ].filter(linkConfig => linkConfig)
+        .map(({ label, href }) => {
+            return <li key={href}>
+                <Link href={href}>
+                    <a className="nav-link"> {label} </a>
+                </Link>
+            </li>
+        })
 
     return (
         <nav className='navbar navbar-light bg-light'>
